4521e45ca71e5b409e5f38ee45a0deb3473a0187
Jeff Glanville <jeff.glanville@outlook.com>
2019-12-05 07:59:01 -0600

    update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+let headLogo = document.getElementByClassName('logo-heading');
+headLogo.addEventListener('mouseover', (event) => {
+    event.target.style.color = 'yellow';
+});
