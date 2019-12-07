9c591b28b7ad130b905db6b255a5b871f14205bb
Jeff Glanville <jeff.glanville@outlook.com>
2019-12-06 18:26:31 -0600

    add branch

--- js/index.js
+++ js/index.js
@@ -1,5 +1,34 @@
 // Your code goes here
-let headLogo = document.getElementByClassName('logo-heading');
+let headLogo = document.querySelector('.logo-heading');
 headLogo.addEventListener('mouseover', (event) => {
-    event.target.style.color = 'yellow';
+    event.target.style.color = 'gold';
 });
+
+headLogo.addEventListener('mouseout', (event) => {
+    event.target.style.color = 'black';
+})
+
+let clickA = document.querySelectorAll('.main-navigation .nav-link');
+clickA.forEach(item => {
+    item.addEventListener('click', (event) => {
+        event.target.style.color = 'blue';
+    })
+})
+
+let clickB = document.querySelectorAll('.main-navigation .nav-link');
+clickB.forEach(item => {
+    item.addEventListener('dblclick', (event) => {
+        event.target.style.color = 'black';
+    })
+})
+
+let anime = document.querySelector('[src="img/fun.jpg"]')
+anime.addEventListener('mouseenter', (event) => {
+    event.target.style.transform = ("rotate(90deg) translateX(100px)")
+})
+
+let anime2 = document.querySelector('[src="img/fun.jpg"]')
+anime.addEventListener('mouseleave', (event) => {
+    event.target.style.transform = ("rotate(0deg) translateX(0px)")
+})
+
